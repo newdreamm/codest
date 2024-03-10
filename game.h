d0aba80cfa3b2e1f3d67416291286915398fd691
dreamcode <17860901021@163.com>
2024-03-10 19:30:03 +0800

    三子棋

---
+++ game.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ROW 3
+#define COL 3
+
+//初始化棋盘
+void InitBoard(char board[ROW][COL], int row, int col);
+//打印棋盘
+void DispalyBoard(char board[ROW][COL],int row,int col);
+//玩家下棋
+void PlayerMove(char board[ROW][COL],int row,int col);
+//电脑下棋
+void ComputerMove(char board[ROW][COL],int row,int col);
+//判断输赢
+char Iswin(char board[ROW][COL],int row,int col);
